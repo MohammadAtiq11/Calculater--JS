4ee7ccc0e747737c564d988d0bb0c043656904c4
Mohammad Atiq <154668693+MohammadAtiq11@users.noreply.github.com>
2024-05-22 22:46:41 +0500

    Calculater -JS

---
+++ app.js
@@ -0,0 +1,17 @@
+let num1 = +prompt("Enter Your 1st Value");
+let num2 = +prompt("Enter Your 2nd Value");
+
+sum = num1 + num2;
+document.write("<h2/>", " +Summation: ", num1, " + ", num2, " = ", sum, "<br>");
+
+sub = num1 - num2;
+document.write("<h2/>", " -Subtraction: ", num1, " - ", num2, " = ", sub, "<br>");
+
+multiply = num1 * num2;
+document.write("<h2/>", " *Multiplication: ", num1, " * ", num2, " = ", multiply, "<br>");
+
+div = num1 / num2;
+document.write("<h2/>", " /Division: ", num1, " / ", num2, " = ", div, "<br>");
+
+reminder = num1 % num2;
+document.write("<h2/>", "  Reminder: ", reminder, "<br>");
